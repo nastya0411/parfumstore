7e3f7a85ae0818ac1685a2285f14cd7791cbeee5
panovanastya041103@gmail.com <panovanastya041103@gmail.com>
2025-05-18 13:42:30 +0300

    Фавикон

--- config/db.php
+++ config/db.php
@@ -2,14 +2,14 @@
 
 return [
     'class' => 'yii\db\Connection',    
-    'dsn' => 'mysql:host=127.0.0.1;dbname=diplom_parfum',
-    'username' => 'parfum',
-    'password' => '5R9-Brq-uth-dda',
-    'charset' => 'utf8',
-    // 'dsn' => 'mysql:host=localhost;dbname=diplom_parfum',
-    // 'username' => 'root',
-    // 'password' => '',
+    // 'dsn' => 'mysql:host=127.0.0.1;dbname=diplom_parfum',
+    // 'username' => 'parfum',
+    // 'password' => '5R9-Brq-uth-dda',
     // 'charset' => 'utf8',
+    'dsn' => 'mysql:host=localhost;dbname=diplom_parfum',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
