1ed2dba6464a56d029d5c89c5fdef510be951a40
evghn <evghn94_git@mail.ru>
2025-05-18 23:22:31 +0300

    конфиг бд

--- config/db.php
+++ config/db.php
@@ -1,12 +1,20 @@
 <?php
-
+if ($_SERVER["SERVER_NAME"] == "parfumstore.infobox.vip") {
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=diplom_parfum',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+        // Schema cache options (for production environment)
+        'enableSchemaCache' => true,
+        'schemaCacheDuration' => 60,
+        'schemaCache' => 'cache',
+    ];
+}
 return [
-    'class' => 'yii\db\Connection',    
-    // 'dsn' => 'mysql:host=127.0.0.1;dbname=diplom_parfum',
-    // 'username' => 'parfum',
-    // 'password' => '5R9-Brq-uth-dda',
-    // 'charset' => 'utf8',
-    'dsn' => 'mysql:host=localhost;dbname=diplom_parfum',
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=MariaDB-11.2;dbname=diplom_parfum',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
