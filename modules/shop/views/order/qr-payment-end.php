f5a76fa011d763b476d11b20641253bdc6d28248
Your Name <you@example.com>
2025-06-24 21:51:31 +0300

    fix error

--- modules/shop/views/order/qr-payment-end.php
+++ modules/shop/views/order/qr-payment-end.php
@@ -11,7 +11,7 @@ $url = "https://api.qrserver.com/v1/create-qr-code/?data=$url&size=250x250";
 
 <div class="d-flex flex-column justify-content-center align-content-center m-auto border border-1 p-3 border-warning" style=" width: 300px; --bs-border-opacity: .8; transition: all 1s ease">
     <div class="mb-3">
-        Зазак №<?= $model->id ?> от <?= Yii::$app->formatter->asDate($model->created_at, "php:d.m.Y") ?>
+        Заказ №<?= $model->id ?> от <?= Yii::$app->formatter->asDate($model->created_at, "php:d.m.Y") ?>
     </div>
 
     <div class="text-warning fw-bold fs-3 text-center">
